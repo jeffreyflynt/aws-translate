c29dd70b96937caebfdf74b89d97ef0e9c790be1
Jeff Flynt <jeffrey.flynt@utexas.edu>
2021-08-11 19:00:32 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # aws-translate
+
+Code for Medium Post - https://jeffreyflynt02.medium.com/translating-user-generated-content-in-real-time-c0fe2165be99
+
+`index.js` - Persisting Translation to Database
+`translateToFile.js` - Output Translations to File
+
+`npm install aws-sdk` or `yarn add aws-sdk`
---
+++ translateToFile.js
@@ -0,0 +1,81 @@
+const AWS = require("aws-sdk");
+const fs = require("fs");
+
+const translate = new AWS.Translate({
+  accessKeyId: "XXXXXXXX",
+  secretAccessKey: "XXXXXXX",
+  region: "us-xxxx-x",
+});
+
+const languages = [
+  "fr",
+  "ar",
+  "zh",
+  "de",
+  "it",
+  "pt",
+  "ru",
+  "uk",
+  "hi",
+  "fi",
+  "el",
+  "hu",
+  "id",
+  "ms",
+  "sv",
+  "th",
+  "tr",
+  "vi",
+  "ko",
+  "ja",
+  "bg",
+  "pl",
+  "no",
+];
+
+const userPost = {
+  _id: "dDGbSZvdNyL4jf2HK",
+  title: "My Post!",
+  content: "I went fishing today and let me tell you, those fish were nothing to play with!",
+};
+
+const translateCall = async (data) => {
+  const params = {
+    SourceLanguageCode: "auto",
+    TargetLanguageCode: data.lang,
+    Text: data.text,
+  };
+
+  try {
+    const translationData = await translate.translateText(params).promise();
+    return translationData;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const translatePosts = async () => {
+  for (let i = 0; i < languages.length; i++) {
+    const userTitleParams = {
+      lang: languages[i],
+      text: userPost.title,
+    };
+    const userContentParams = {
+      lang: languages[i],
+      text: userPost.content,
+    };
+
+    const translatedTitle = await translateCall(userTitleParams);
+    const translatedContent = await translateCall(userContentParams);
+
+    const data = {
+      lang: languages[i],
+      title: translatedTitle.TranslatedText,
+      content: translatedContent.TranslatedText,
+    };
+
+    fs.writeFileSync("./output/" + languages[i] + ".json", data);
+  }
+};
+
+translatePosts();
